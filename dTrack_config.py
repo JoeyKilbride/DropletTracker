2800af8ee298f5955ad4ae318576bd8ae8803345
JoeyKilbride <joeykilbride24@gmail.com>
2024-10-04 10:52:00 +0100

    Adding missing required variables in config example

--- dTrack_config.py
+++ dTrack_config.py
@@ -2,3 +2,9 @@ filename = "Results" # tracking information text file
 prefix = "name" # output label
 centres = "all" # droplets: "all", manuals: [[x1,y1],[x2,y2]...]
 export = True # Save results?
+
+calibration = 1 # m/px
+start = 1 # starting frame
+end = 10 # ending frame
+steps = 1 # analyse every specified frame
+FPS = 1 # frames per second
