ff8d8e335c3ec4a4a9a0a218096f72cbb9ab375e
JoeyKilbride <joeykilbride24@gmail.com>
2024-12-18 17:01:44 +0000

    updating config to default to correct value

--- dTrack_config.py
+++ dTrack_config.py
@@ -1,6 +1,6 @@
 filename = "Results" # tracking information text file
 prefix = "name" # output label
-centres = "all" # droplets: "all", manuals: [[x1,y1],[x2,y2]...]
+centres = "droplet" # droplets: "droplets", manuals: [[x1,y1],[x2,y2]...]
 export = True # Save results?
 
 calibration = 1 # m/px
